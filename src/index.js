30e8596ae7b754876d36c291d92edc247f7655e0
Evgeniya Bayda <baje75128@gmail.com>
2021-08-24 15:36:22 +0300

    copy

---
+++ src/index.js
@@ -0,0 +1,39 @@
+// import './sass/main.scss';
+import refs from './js/refs';
+const axios = require('axios');
+
+let searchValue = '';
+function onSearchCocktail(e) {
+  e.preventDefault();
+  searchValue = refs.input.value;
+  //   console.log(refs.input.value);
+  axios
+    .get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue}`)
+    .then(result => {
+      renderGallery(result.data.drinks);
+      refs.form.reset();
+    })
+    .catch(error => console.log(error));
+}
+refs.form.addEventListener('submit', onSearchCocktail);
+
+function markUpItem({ strDrink, strDrinkThumb }) {
+  const article = `<li class='gallery-item'><img class='gallery-img' src='${strDrinkThumb}' alt='${strDrink}'/><p class ="gallery-text">${strDrink}</p></li>`;
+  refs.gallery.insertAdjacentHTML('beforeend', article);
+}
+
+function renderGallery(arr) {
+  arr.forEach(el => markUpItem(el));
+}
+
+// async function onFetchCocktails(searchValue) {
+//   try {
+//     const responseUrl = await fetch(
+//       `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchValue}`,
+//     );
+//     const cocktail = await responseUrl.json();
+//     return cocktail;
+//   } catch (error) {
+//     console.log('Error!');
+//   }
+// }
---
+++ src/js/refs.js
@@ -0,0 +1,5 @@
+export default {
+  gallery: document.querySelector('#js-gallery'),
+  input: document.querySelector('.input'),
+  form: document.querySelector('#search-form'),
+};
